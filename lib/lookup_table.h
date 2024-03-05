f8bcaff434fad9b845c91903cf4b816b5be289fb
lncarpe4 <lncarpe4@ncsu.edu>
2024-03-05 10:09:01 -0500

    Resolving compilation errors

--- lib/lookup_table.h
+++ lib/lookup_table.h
@@ -1,7 +1,9 @@
 #include <openssl/aes.h>
 #include <sys/random.h>
 #include <assert.h>
+#include <string.h>
 #include "memory_mosq.h"
 #include "mosquitto_internal.h"
+#include "uthash.h"
 
 uint8_t *checkLookupTable(uint8_t *payload, size_t p_len /*, uint8_t *topic*/);
